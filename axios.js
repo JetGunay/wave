e1674e2a2a4bdf7162cb1aa947c33d40d5d1bef5
Jet Gunay <47313684+JetGunay@users.noreply.github.com>
2022-10-20 18:58:54 +0800

    Update axios.js

--- axios.js
+++ axios.js
@@ -7,15 +7,15 @@ headers.append('Accept', 'application/json');
 headers.append('Access-Control-Allow-Origin', 'http://localhost:3000');
 headers.append('Access-Control-Allow-Credentials', 'true');
 headers.append('GET', 'POST', 'OPTIONS');
-// headers.append('Authorization', 'Bearer eyJ0dCI6InAiLCJhbGciOiJIUzI1NiIsInR2IjoiMSJ9.eyJkIjoie1wiYVwiOjM4NDYzOTMsXCJpXCI6ODMzMTUyMixcImNcIjo0NjMyODc0LFwidVwiOjk1OTM4MTIsXCJyXCI6XCJVU1wiLFwic1wiOltcIldcIixcIkZcIixcIklcIixcIlVcIixcIktcIixcIkNcIixcIkRcIixcIk1cIixcIkFcIixcIkxcIixcIlBcIl0sXCJ6XCI6W10sXCJ0XCI6MH0iLCJpYXQiOjE2NjAyMTQ5NjR9.Cki7YlfsQrZH36ThVLzncKMWU9fzZhlgmNx6lQ9iZkQ');
-headers.append('Authorization', 'Bearer eyJ0dCI6InAiLCJhbGciOiJIUzI1NiIsInR2IjoiMSJ9.eyJkIjoie1wiYVwiOjM4NDYzOTMsXCJpXCI6ODQxNDU3NixcImNcIjo0NjM1ODE0LFwidVwiOjk1OTM4MTIsXCJyXCI6XCJVU1wiLFwic1wiOltcIldcIixcIkZcIixcIklcIixcIlVcIixcIktcIixcIkNcIixcIkRcIixcIk1cIixcIkFcIixcIkxcIixcIlBcIl0sXCJ6XCI6W10sXCJ0XCI6MH0iLCJpYXQiOjE2NjYyNTUzMDh9.8OCwkj8xXpvsAsky9zoKn-vjKDmtYBT3rQ3UTIEX6yw');
+// headers.append('Authorization', 'Bearer '+token);
+headers.append('Authorization', 'Bearer '+token);
 
 const axiosClient = axios.create({
   baseURL: `https://www.wrike.com/api/v4/`,
   // headers: headers
   headers: {
     Authorization:
-      'Bearer eyJ0dCI6InAiLCJhbGciOiJIUzI1NiIsInR2IjoiMSJ9.eyJkIjoie1wiYVwiOjM4NDYzOTMsXCJpXCI6ODQxNDU3NixcImNcIjo0NjM1ODE0LFwidVwiOjk1OTM4MTIsXCJyXCI6XCJVU1wiLFwic1wiOltcIldcIixcIkZcIixcIklcIixcIlVcIixcIktcIixcIkNcIixcIkRcIixcIk1cIixcIkFcIixcIkxcIixcIlBcIl0sXCJ6XCI6W10sXCJ0XCI6MH0iLCJpYXQiOjE2NjYyNTUzMDh9.8OCwkj8xXpvsAsky9zoKn-vjKDmtYBT3rQ3UTIEX6yw',
+      'Bearer '+token,
   },
 });
 
